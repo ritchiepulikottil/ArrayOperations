3f40c5a6dad2f097d193e0f7e7dfa1ccce1a6f58
Ritchie Pulikottil <48920595+ritchiepulikottil@users.noreply.github.com>
2022-06-25 00:28:40 +0530

    Create MyArray.js

---
+++ javascript/MyArray.js
@@ -0,0 +1,17 @@
+const strings= ['a', 'b', 'c', 'd'];
+const numbers = [1,2,3,4,5];
+
+//push
+strings.push('e');
+
+//pop
+strings.pop();
+strings.pop();
+
+//unshift
+strings.unshift('x')
+
+//splice
+strings.splice(2, 0, 'alien');
+
+console.log(strings)
