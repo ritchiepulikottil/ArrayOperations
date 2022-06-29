a7e8ede05db3d187914978e64689fccd44fa0969
Ritchie Pulikottil <48920595+ritchiepulikottil@users.noreply.github.com>
2022-06-29 07:37:46 +0530

    Update MyArray.cs

--- c#/MyArray.cs
+++ c#/MyArray.cs
@@ -1,81 +1,46 @@
-using System;
+//The following code snippet defines the simplest dynamic array of integer types that do not have a fixed size.
+int[] intArray;
 
-class MyArray
-    {
-        public int length;
-        private Object[] data;
-        //creating constructors
-        public MyArray()
-        {
-            this.length = 0;
-            this.data = new Object[1];
-        }
+//The following code snippet declares an array that can store 5 items only starting from index 0 to 4. 
+int[] intArray;
+intArray = new int[5];
 
-        public Object get(int index)
-        {
-            return data[index];
-        }
-    
-        //pushing items at last index
-        public Object[] push(Object item)
-        {
-            if (this.data.Length == this.length)
-            {
-                Object[] temp = new Object[this.length]; // creates a temp array 
-                Array.Copy(this.data, temp, length); //copies all data to temp array     
-                this.data = new Object[length + 1]; //increases size of temp array
-                Array.Copy(temp, this.data, length);
-            }
-            this.data[this.length] = item;  //inserts item into last index
-            this.length++; 
-            return this.data;
-        }
+//The following code snippet declares an array that can store 100 items starting from index 0 to 99. 
+int[] intArray;
+intArray = new int[100];
 
-        public Object pop()
-        {
-            Object poped = data[this.length - 1];
-            this.data[this.length - 1] = null;  //nulling out the last item
-            this.length--; //decresing the length by 1 index            
-            return poped; //returning the poped array
-        }
+//The following code snippet defines arrays of double, char, bool, and string data types. 
+double[] doubleArray = new double[5];
+char[] charArray = new char[5];
+bool[] boolArray = new bool[2];
+string[] stringArray = new string[10];
 
-        public Object delete(int index)
-        {
-            Object itemToDelete = data[index];
-            shiftItems(index); //calling the shiftItems method to shift the items at index
-            return itemToDelete;
-        }
+//The following code snippet creates an array of 3 items and values of these items are added when the array is initialized. 
+// Initialize a fixed array
+int[] staticIntArray = new int[3] {1, 3, 5};
 
-        private void shiftItems(int index)
-        {
-            for (int i = index; i < length - 1; i++)
-            {
-                data[i] = data[i + 1];
-            }
-            data[length - 1] = null;
-            length--;
-        }
+//The following code snippet declares a dynamic array with string values. 
+// Initialize a dynamic array items during declaration
+string[] strArray = new string[] { "Ritchie", "John", "Chuck"};
 
-        static void Main(string[] args)
-        {
-            MyArray myArray = new MyArray();
+//We can access an array item by passing the item index in the array. 
+//The following code snippet creates an array of three items and displays those items on the console. 
+// Initialize a fixed array one item at a time
+int[] staticIntArray = new int[3];
+staticIntArray[0] = 1;
+staticIntArray[1] = 3;
+staticIntArray[2] = 5;
+// Read array items one by one
+Console.WriteLine(staticIntArray[0]);
+Console.WriteLine(staticIntArray[1]);
+Console.WriteLine(staticIntArray[2]);
 
-            myArray.push("Hello");
-            myArray.push("World");
-            myArray.push("!");
+//C# Adding and Removing elements to an array with an existing size
+//You can't do that with arrays in C# without allocating a new array. Because arrays are fixed in size.
+//If you want to be able to add/remove elements from a container, you could use List<T>. 
+//Alternativly you could use an ArrayList but that is not recommended, since in most cases List<T> has a performance advantage.
+List<string> food = new List<string> { "Bacon", "Cheese", "Patty", "Crabs" };
+food.Add("Milk"); //Will add Milk to the list
+food.Remove("Bacon"); //Will remove "Bacon"
 
-            //for (int i = 0; i < myArray.length; i++)
-            //{
-            //    Console.WriteLine(myArray.get(i));
-            //}
 
-            //myArray.pop();
-
-            myArray.delete(1);
-
-            for (int i = 0; i < myArray.length; i++)
-            {
-                Console.WriteLine(myArray.get(i));
-            }
-        }
-    }
